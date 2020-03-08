eadbecfb6421c1984cb1741aee7f86af5858c00a
padureanbianca <61680493+padureanbianca@users.noreply.github.com>
2020-03-08 22:37:16 +0200

    Tema Lab 3
    Interschimbare bit wise

---
+++ L3/Source.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#include<ctype.h>
+
+void bitWise(int a, int b) {
+	/* Rezolvare cu bit wise */
+	if (a != b) {
+		a = a ^ b;
+		b = a ^ b;
+		a = a ^ b;
+	}
+	printf("a = %d, b = %d\n", a, b);
+}
+
+int main()
+{
+	int a;
+	int b;
+
+		printf("Introduceti numerele: ");
+		printf("\na = ");
+		scanf("%d", &a);
+		printf("b = ");
+		scanf("%d", &b);
+		printf("Interschimbarea s-a realizat: \n"); 
+		bitWise(a, b);
+		
+	system("pause");
+	return 0;
+}
