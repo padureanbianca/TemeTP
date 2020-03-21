28fd8abecfd11893f4320e28681590d2016e253e
padureanbianca <61680493+padureanbianca@users.noreply.github.com>
2020-03-21 12:39:49 +0200

    Add files via upload

---
+++ L4/Source.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#include<ctype.h>
+
+int main()
+{
+	int* v;
+	v = (int*)(malloc(2 * sizeof(int)));
+	*v = 0;
+	*(v + 1) = 0;
+	printf("Introduceti elementele vectorului:\n");
+	while (scanf("%d", v)==1) {
+		*(v + 1) += *v;
+		//printf("\nSuma elementelor este: %d\n", *(v + 1));
+	}
+	printf("Suma elementelor este: %d\n", *(v + 1));
+
+	system("pause");
+	return 0;
+}
+
